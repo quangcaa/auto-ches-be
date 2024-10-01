69c7bbbfa24db729b6e6e24a062b58c97672f70b
AnNgoexe <quocan2004tp@gmail.com>
2024-10-01 12:47:15 +0700

    fixing routes in profile API

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 const ProfileController = require('../controllers/ProfileController');
 
+// Route để lấy thông tin hồ sơ người dùng
 router.get('/bio', ProfileController.getUserProfileById);
+
+// Route để cập nhật thông tin hồ sơ người dùng
 router.put('/bio', ProfileController.updateUserProfile);
+
+// Route để lấy danh sách bạn bè của người dùng
+router.get('/friends', ProfileController.getFriend);
+
+// Route để lấy danh sách yêu cầu kết bạn đang chờ xử lý của người dùng
+router.get('/pending-requests', ProfileController.getPendingFriendRequests);
+
+// Route để lấy danh sách yêu cầu kết bạn từ người khác gửi đến người dùng
+router.get('/invitations', ProfileController.getFriendRequestsFromOthers);
 
 module.exports = router;
