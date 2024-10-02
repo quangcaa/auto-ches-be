328deb0bef4e8d79b9b352b3c55b54a911a112c2
Maou <nguyenvietquang.1601@gmail.com>
2024-10-02 19:53:26 +0700

    changePassword

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const NODE_ENV = process.env.NODE_ENV
 
 app.use(express.json()) // allow parse req.body
 app.use(cookieParser()) // allow parse cookies
+// app.use(express.urlencoded({ extended: true })) // allow parse URL-encoded
 
 route(app) // routes init
 
