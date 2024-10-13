65beb6484d4d9f2e3bc6fd4b7e1f3a9c678687c3
AnNgoexe <quocan2004tp@gmail.com>
2024-10-13 22:54:45 +0700

    create socket chat

---
+++ socket/socket.js
@@ -0,0 +1,37 @@
+const { Server } = require('socket.io');
+const http = require('http');
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+
+const CLIENT_PORT = 5173;
+const io = new Server(server, {
+    cors: {
+        origin: `${process.env.CLIENT_URL}:${CLIENT_PORT}`,
+        methods: ["GET", "POST"],
+    },
+});
+
+const userSocketMap = {};
+
+const getSocketIdByUserId = (userId) => {
+    return userSocketMap[userId] || null;
+};
+
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    }
+
+    socket.on('disconnect', () => {
+        delete userSocketMap[userId];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
+
+module.exports = { io, app, server, getSocketIdByUserId };
