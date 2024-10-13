2fc46287bd0cea7e792377ec11b46f07f8a39b44
AnNgoexe <quocan2004tp@gmail.com>
2024-10-13 22:57:17 +0700

    send message route

--- routes/inbox.js
+++ routes/inbox.js
@@ -7,5 +7,6 @@ const isAuth = require('../middlewares/isAuth')
 router.get('/', isAuth, InboxController.getAllInbox)
 router.get('/:userId', isAuth, InboxController.getInboxMessage)
 router.delete('/delete-inbox/:userId', isAuth, InboxController.deleteInbox)
+router.post('/send-message/:userId', isAuth, InboxController.sendMessage)
 
 module.exports = router
