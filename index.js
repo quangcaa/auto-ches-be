f7db3dad2c63a3eb1aa1885c059142117a20bb39
AnNgoexe <quocan2004tp@gmail.com>
2024-10-15 00:02:30 +0700

    add socket to app

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const express = require('express')
 const cookieParser = require('cookie-parser')
+const {app, server} = require("./socket/socket.js")
 
 const route = require('./routes/index')
 
 dotenv.config()
 
-const app = express()
-
 const PORT = process.env.PORT
 const NODE_ENV = process.env.NODE_ENV
 
+app.use(cors()) // enable cors for all routes 
 app.use(express.json()) // allow parse req.body
 app.use(cookieParser()) // allow parse cookies
 // app.use(express.urlencoded({ extended: true })) // allow parse URL-encoded
 
 route(app) // routes init
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running in ${NODE_ENV} mode on port ${PORT}`)
 })
